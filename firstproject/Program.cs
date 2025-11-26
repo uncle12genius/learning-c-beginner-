2ba8ae6da6e932c9b82801c46c6cfa6f7fbc11cf
uncle12genius <omondievan461@gmail.com>
2025-11-26 18:39:54 +0300

    learing c#

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -1,2 +1,19 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+    namespace ProgramApp  {
+    class Program { 
+    static void Main(String[]args)
+        {
+            int number = 600;
+            string firstName = "evan ";
+            char last_name = 'a';
+            bool value = false;
+
+            Console.WriteLine(number ); 
+            Console.WriteLine ( firstName );
+            Console.WriteLine (last_name);
+            Console.WriteLine(value);
+            Console.WriteLine("john"  +  firstName );
+        }
+    }
+}
